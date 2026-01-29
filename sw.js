28a15354514a854f7bd91d3d93a224bf162a4eae
21programado <21programado@gmail.com>
2026-01-29 10:42:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,16 @@
-const CACHE_NAME = "facturas-v3";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE_NAME = 'gastos-v1';
+const urlsToCache = ['/', '/index.html'];
 
-// Instalar y cachear
-self.addEventListener("install", event => {
-  self.skipWaiting();
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activar y limpiar caches viejos
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch: cache primero, red después
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
